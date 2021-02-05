981311e5c1325cf7bb90c2edb59e9d3bea665576
sjoon2455 <sjoon2455@kaist.ac.kr>
2021-02-05 11:46:27 +0900

    various dataset but only 20 now

---
+++ attacks_backup.py
@@ -0,0 +1,48 @@
+ attacks = [
+        # L1
+        fa.EADAttack(),
+        fa.L1BrendelBethgeAttack(),
+
+        # L_2
+        fa.L2ContrastReductionAttack(),
+        fa.L2ProjectedGradientDescentAttack(),
+        fa.L2AdditiveGaussianNoiseAttack(),
+        fa.L2AdditiveUniformNoiseAttack(),
+        fa.L2ClippingAwareAdditiveGaussianNoiseAttack(),
+        fa.L2ClippingAwareAdditiveUniformNoiseAttack(),
+        fa.L2FastGradientAttack(),
+        fa.L2RepeatedAdditiveGaussianNoiseAttack(),
+        fa.L2RepeatedAdditiveUniformNoiseAttack(),
+        fa.L2ClippingAwareRepeatedAdditiveGaussianNoiseAttack(),
+        fa.L2ClippingAwareRepeatedAdditiveUniformNoiseAttack(),
+        fa.L2DeepFoolAttack(),
+        fa.L2BrendelBethgeAttack(),
+        fa.L2PGD(),
+        fa.L2CarliniWagnerAttack(),
+        fa.DDNAttack(),
+
+        # L_inf
+        fa.LinfProjectedGradientDescentAttack(),
+        fa.LinfBasicIterativeAttack(),
+        fa.LinfFastGradientAttack(),
+        fa.LinfAdditiveUniformNoiseAttack(),
+        fa.LinfRepeatedAdditiveUniformNoiseAttack(),
+        fa.LinfDeepFoolAttack(),
+        fa.LinfinityBrendelBethgeAttack(),
+        fa.LinfPGD(),
+
+        fa.InversionAttack(),  # invert pixel
+        fa.BinarySearchContrastReductionAttack(),
+        fa.LinearSearchContrastReductionAttack(),
+        fa.GaussianBlurAttack(),
+        fa.SaltAndPepperNoiseAttack(),  # until misclassified
+        fa.LinearSearchBlendedUniformNoiseAttack(),
+        fa.BoundaryAttack(),  # reduce perturbation while staying adversarial
+        # fa.BinarizationRefinementAttack(), #for models that preprocess inputs by binarizing the inputs
+        # fa.DatasetAttack(), # needs secondary dataset to be drawn from
+
+        # gradient
+        fa.L0BrendelBethgeAttack(),
+        fa.NewtonFoolAttack(),
+        # fa.VirtualAdversarialAttack(),
+    ]
--- main.py
+++ main.py
@@ -12,11 +12,9 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-if __name__ == "__main__":
-    batch = 20
 
+def run(dataset, batch=20):
     class LeNet(nn.Module):
-
         def __init__(self):
             super(LeNet, self).__init__()
             self.conv1 = nn.Conv2d(1, 20, 5, 1)
@@ -44,15 +42,25 @@ if __name__ == "__main__":
 
     images, labels = ep.astensors(
         *samples(fmodel, dataset="mnist", batchsize=batch))
+    # print(type(images), type(labels))
+    # print(images[0], labels[0])
     # clean_acc = accuracy(fmodel, images, labels)
     # print(f"clean accuracy:  {clean_acc * 100:.1f} %")
 
     attacks = [
-        fa.FGSM(),
-        fa.LinfPGD(),
+        fa.L2FastGradientAttack(),
+        fa.L2DeepFoolAttack(),
+        fa.L2CarliniWagnerAttack(),
+        fa.DDNAttack(),
+
+        # L_inf
         fa.LinfBasicIterativeAttack(),
-        fa.LinfAdditiveUniformNoiseAttack(),
+        fa.LinfFastGradientAttack(),
         fa.LinfDeepFoolAttack(),
+        fa.LinfPGD(),
+
+        # fa.BoundaryAttack(),  # reduce perturbation while staying adversarial
+
     ]
     epsilons = [
         # 0.0,
@@ -73,8 +81,11 @@ if __name__ == "__main__":
 
     attacks_result = [[0 for _ in range(len(attacks))]
                       for _ in range(len(attacks))]
-    total_recovered = 0
+    drop_result = [[0 for _ in range(len(attacks))]
+                   for _ in range(len(attacks))]
+    total_drop, total_recovered = 0, 0
     for n, attack_1 in enumerate(attacks):
+        print(n, str(attack_1)[:8])
         for m, attack_2 in enumerate(attacks):
             # which needs to be the same as label
             ori_predictions = fmodel(images).argmax(axis=-1)
@@ -90,22 +101,42 @@ if __name__ == "__main__":
             double_adv_predictions = fmodel(
                 raw_double_advs).argmax(axis=-1)
 
-            drop, recovered = 0, 0
+            attack_fail_drop, predict_fail_drop, recovered = 0, 0, 0
             ori_predictions = ori_predictions.raw.numpy()
             adv_predictions = adv_predictions.raw.numpy()
             double_adv_predictions = double_adv_predictions.raw.numpy()
 
             for i in range(batch):
-                # print(str(attack_1)[:8], str(attack_2)[:8], ori_predictions[i], adv_predictions[i],
-                #   double_adv_predictions[i])
-                if ori_predictions[i] == adv_predictions[i]:
-                    drop += 1
-                    continue  # attack failed at all
+                # if n == 3 or n == 4:
+                # print(str(attack_1)[:8], str(attack_2)[
+                #       :8], ori_predictions[i], adv_predictions[i], double_adv_predictions[i])
 
-                if ori_predictions[i] == double_adv_predictions[i]:
-                    recovered += 1
+                #  or ori_predictions[i] != labels[i]
+                if ori_predictions[i] == adv_predictions[i]:
+                    attack_fail_drop += 1
+                    continue  # attack failed at all
+                elif ori_predictions[i] != labels[i].raw.numpy():
+                    predict_fail_drop += 1
+                    continue
+                else:
+                    if ori_predictions[i] == double_adv_predictions[i]:
+                        recovered += 1
+            drop = attack_fail_drop + predict_fail_drop
+            drop_result[n][m] = batch-drop
             attacks_result[n][m] = recovered
+            total_drop += drop
             total_recovered += recovered
+    print("-------------------------------------------------------")
+    print("Dataset: ", dataset)
     print("recovery matrix: ", attacks_result)
-    print(total_recovered, batch*len(attacks)**2)
-    print("recovery rate: ", total_recovered/(batch*len(attacks)**2-drop))
+    print(" total  matrix:  ", drop_result)
+    print("  Drop report:   ", attack_fail_drop, predict_fail_drop)
+    print("recovery rate: ", total_recovered /
+          (batch*len(attacks)**2-total_drop))
+    print("-------------------------------------------------------")
+
+
+if __name__ == "__main__":
+    datasets = ['mnist', 'imagenet', 'cifar10', 'cifar100']
+    for dataset in datasets:
+        run(dataset, 20)
